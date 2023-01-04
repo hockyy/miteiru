04f3e25a0ff8602b255fbac57c856b081cbedae3
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2023-01-05 00:02:27 +0700

    Add dropzone UI

--- renderer/components/MiteiruDropzone.tsx
+++ renderer/components/MiteiruDropzone.tsx
@@ -6,18 +6,18 @@ import {Key} from "./KeyboardHelp";
 const ActiveDropzoneCue = ({isActive}) => {
   const message = ["Drag here please UωU", "Drop Here (ᴗ_ ᴗ。)"]
   return <div
-      className={"flex content-center items-center justify-center w-screen h-screen text-white text-5xl"}>
+      className={"flex content-center items-center justify-center w-screen h-screen text-orange-200 text-5xl"}>
     <div
         style={{
           WebkitTextStrokeColor: "black",
           WebkitTextStrokeWidth: "1px",
           fontFamily: "Arial"
         }}
-        className={"font-bold flex flex-col gap-2 justify-center items-center rounded-lg border-[10px] border-dashed w-screen h-screen"}>
+        className={"font-bold flex flex-col gap-2 justify-center items-center rounded-3xl border-[10px] border-dashed w-screen h-screen border-orange-500"}>
 
       <div>{message[+isActive]}</div>
       <div className={"flex flex-row justify-center items-center"}>
-        or Press <Key extraClass={"mx-4 text-gray-500"} value={"X"}/> to toggle me~!
+        or Press <Key extraClass={"mx-4 text-orange-500"} value={"X"}/> to toggle me~!
       </div>
     </div>
   </div>
@@ -48,13 +48,14 @@ export const MiteiruDropzone = ({setPrimarySub, setSecondarySub, setVideoSrc, me
   }, [])
 
   const {getRootProps, getInputProps, isDragActive} = useDropzone({
+    noClick: true,
     onDrop,
   })
 
   return (<div>
     <div {...getRootProps()} style={{
       zIndex: 20,
-      backgroundColor: "rgba(122,122,122,0.45)",
+      backgroundColor: "rgba(171,248,255,0.45)",
       position: "fixed",
       top: "0vh",
       height: "100vh",
