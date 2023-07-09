e52eb926b3e88d484b1e4b198730c60524803578
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2023-07-10 02:44:44 +0700

    Fix subtitle goes first when seeking to end

--- renderer/hooks/usePlayNextAfterEnd.tsx
+++ renderer/hooks/usePlayNextAfterEnd.tsx
@@ -1,6 +1,11 @@
 import {useEffect} from "react";
 
-export const usePlayNextAfterEnd = (player, currentTime, onVideoEndHandler, duration, changeTimeTo, setEnableSeeker) => {
+export const usePlayNextAfterEnd = (player,
+                                    currentTime,
+                                    onVideoEndHandler,
+                                    duration,
+                                    changeTimeTo,
+                                    setEnableSeeker) => {
   useEffect(() => {
     if (player && currentTime * 1000 === duration && duration > 0) {
       setEnableSeeker(false);
