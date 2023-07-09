a7253be23f9cec6b920fb8513bf92b21b0bd28dd
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2023-07-10 02:48:20 +0700

    Remove unused vars

--- renderer/hooks/usePlayNextAfterEnd.tsx
+++ renderer/hooks/usePlayNextAfterEnd.tsx
@@ -4,12 +4,11 @@ export const usePlayNextAfterEnd = (player,
                                     currentTime,
                                     onVideoEndHandler,
                                     duration,
-                                    changeTimeTo,
                                     setEnableSeeker) => {
   useEffect(() => {
     if (player && currentTime * 1000 === duration && duration > 0) {
       setEnableSeeker(false);
       onVideoEndHandler();
     }
-  }, [player, currentTime, duration, changeTimeTo]);
+  }, [player, currentTime, duration]);
 }
