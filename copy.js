f7f0a788b254e1e74ce04d26774388967ee4fcee
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2023-07-25 20:57:05 +0700

    [#34] Add copy.js scripts

---
+++ copy.js
@@ -0,0 +1,9 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+const sourceDir = path.join(__dirname, 'renderer', 'public');
+const destDir = path.join(__dirname, 'app');
+
+fs.copy(sourceDir, destDir)
+  .then(() => console.log('Files copied successfully!'))
+  .catch(err => console.error(err));
