56b74774d4532bdd5dbd834ffb5fccd4debaa57b
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2023-07-27 23:38:27 +0700

    [#27] Add verb appender

--- renderer/hooks/useMiteiruTokenizer.tsx
+++ renderer/hooks/useMiteiruTokenizer.tsx
@@ -15,9 +15,36 @@ const useMiteiruTokenizer = (): { tokenizeMiteiru: (sentence: string) => Promise
     if (tokenizerMode === 'kuromoji') {
       const kuromojiEntries = await ipcRenderer.invoke('tokenizeUsingKuromoji', sentence)
       res = processKuromojinToSeparations(kuromojiEntries);
-      return res;
-    } else if(tokenizerMode !== '') {
+    } else if (tokenizerMode !== '') {
       res = getFurigana(sentence, tokenizerMode);
+    }
+    for (let i = 0; i < res.length - 1; i++) {
+      const entry = res[i];
+      if (entry.pos.split('-').includes("動詞")) {
+        const taken = ['助動詞', '助詞']
+        const ignorable = ['記号', '名詞']
+
+        let nextPos;
+        let accum = [entry];
+        let itr = i + 1;
+        do {
+          nextPos = res[itr].pos.split('-')[0]
+          if (!ignorable.includes(nextPos)) {
+            accum.push(res[itr]);
+            if (!taken.includes(nextPos)) {
+            }
+          } else break;
+          itr++;
+        } while (itr < res.length);
+        console.log('--------')
+        console.log(accum);
+        const appended = accum.reduce((pre, curval) => {
+          return pre + curval.origin;
+        }, '');
+
+        console.log(appended);
+        i = itr - 1;
+      }
     }
     return res;
   }, [tokenizerMode])
