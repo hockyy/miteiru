56fc6b83a25d5e1870a703a41931a53737e1ae54
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2023-07-30 01:39:54 +0700

    [#7] Fix animation when top is radical

--- renderer/components/KanjiVGDisplay.tsx
+++ renderer/components/KanjiVGDisplay.tsx
@@ -6,32 +6,32 @@ const KanjiVGDisplay = ({filename}) => {
   const [svgData, setSvgData] = useState("");
   const [animationKey, setAnimationKey] = useState(0);
   const svgRef = useRef(null);
+  const timeoutIds = useRef([]);
 
   useEffect(() => {
     if (filename) {
       ipcRenderer.invoke("readKanjiSVG", filename).then(val => {
-        const cleanSvgData = val.match(/<svg[^>]*>([\s\S]*?)<\/svg>/i)[0];
-        console.log(cleanSvgData)
-        setSvgData(cleanSvgData);
+        console.log(val)
+        setSvgData(val);
       });
     }
   }, [filename]);
 
   useEffect(() => {
     if (svgData) {
+      // Before setting up new animations, clear all previous ones
+      timeoutIds.current.forEach(id => clearTimeout(id));
+      timeoutIds.current = [];
       const svgContainer = svgRef.current;
+
       // Assuming that svgContainer now refers to the parent g element
       const elementContainer = (svgContainer.children[0].children[0].children[0]);
-      console.log(elementContainer.children)
-      // const groups = elementContainer
 
-      const groups: SVGPathElement[] = Array.from(elementContainer.children);
-      // console.log(groups)
+      const groups: SVGPathElement[] = elementContainer.getAttribute('kvg:radical') ? [elementContainer] : Array.from(elementContainer.children);
       const texts = svgContainer.querySelectorAll('text');
       let strokeIndex = 0;
 
       groups.forEach(group => {
-        console.log(group)
         const color = getRandomColor();
         const paths: SVGPathElement[] = Array.from(group.querySelectorAll('path[id^="kvg"]'));
 
@@ -44,9 +44,12 @@ const KanjiVGDisplay = ({filename}) => {
           path.style.stroke = color;
           path.getBoundingClientRect();
           path.style.transition = "stroke-dashoffset 1s ease-in-out";
-          setTimeout(() => {
+          const timeoutId = setTimeout(() => {
             path.style.strokeDashoffset = "0";
           }, strokeIndex * 1000);
+
+          // Store the timeout ID so you can clear it later if needed
+          timeoutIds.current.push(timeoutId);
           strokeIndex++;
         });
       });
@@ -56,12 +59,31 @@ const KanjiVGDisplay = ({filename}) => {
         text.style.opacity = "0";
         text.getBoundingClientRect();
         text.style.transition = "opacity 1s ease-in-out";
-        setTimeout(() => {
+        const timeoutId = setTimeout(() => {
           text.style.opacity = "1";
         }, index * 1000);
+
+        // Store the timeout ID so you can clear it later if needed
+        timeoutIds.current.push(timeoutId);
       });
     }
   }, [svgData, animationKey]);
+
+  useEffect(() => {
+    if (svgData) {
+      const svgContainer = svgRef.current;
+
+      const svgElement = svgContainer.children[0]
+      const width = svgElement.getAttribute('width');
+      const height = svgElement.getAttribute('height');
+      const scaleFactor = 1.5;  // Replace with your scale factor
+
+      if (width && height) {
+        svgElement.setAttribute('width', parseInt(width) * scaleFactor);
+        svgElement.setAttribute('height', parseInt(height) * scaleFactor);
+      }
+    }
+  }, [svgData])
 
   const handleButtonClick = () => {
     setAnimationKey(prevKey => prevKey + 1);
@@ -77,9 +99,9 @@ const KanjiVGDisplay = ({filename}) => {
   }
 
   return (
-      <div>
+      <div className={'flex flex-col w-auto p-4 gap-4 items-center'}>
         <AwesomeButton type={'primary'} onPress={handleButtonClick}>Repeat Animation</AwesomeButton>
-        <div ref={svgRef} dangerouslySetInnerHTML={{__html: svgData}}/>
+        <div className="svg-container" ref={svgRef} dangerouslySetInnerHTML={{__html: svgData}}/>
       </div>
   );
 };
