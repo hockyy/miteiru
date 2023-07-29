63a24b46396b4eecda4a7d5e18eeae2aa0754e09
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2023-07-30 02:07:48 +0700

    [#7] Add init kanji using zip lfs

---
+++ scripts/initKanjiDic.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const extract = require('extract-zip');
+const path = require("path");
+
+const source = path.join(__dirname, '../archived/kanji.zip');
+const target = path.join(__dirname, '../renderer/public');
+
+fs.access(target, fs.constants.F_OK, (err) => {
+  if (err) {
+    fs.mkdir(target, {recursive: true}, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  }
+});
+
+extract(source, {dir: target})
+.then(() => {
+  console.log('Extraction completed!');
+})
+.catch((err) => {
+  console.log('Extraction failed!', err);
+});
